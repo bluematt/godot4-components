0314e5c43d15a14d8fd2ae8c96b577639c977d1e
bluematt <307723+bluematt@users.noreply.github.com>
2023-02-18 15:29:55 +0000

    Added VisualPlaceholder component
    Added a VisualPlaceholder component.  Updated examples to use this rather than the default project sprite.

--- examples/SimpleStateMachine.gd
+++ examples/SimpleStateMachine.gd
@@ -7,12 +7,12 @@ const COLORS := [
 	Color.WHITE,
 ]
 
-@onready var sprite := get_parent() as Sprite2D
+@onready var entity := get_parent() as Node2D
 
-# When entering idle, set the sprite's modulation to a random color
+# When entering idle, set the entity's modulation to a random color
 # the allowed values.
 func spr_rotate_idle_enter() -> void:
-	sprite.modulate = COLORS.pick_random()
+	entity.modulate = COLORS.pick_random()
 
 # Do nothing when idle.
 func spr_rotate_idle_process(_delta: float) -> void:
@@ -23,13 +23,13 @@ func spr_rotate_idle_unhandled_key_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_select"):
 		transition_to("rotate")
 	
-# Before rotating, set the sprite to HOT_PINK!
+# Before rotating, set the entity to HOT_PINK!
 func spr_rotate_rotate_enter() -> void:
-	sprite.modulate = Color.HOT_PINK
+	entity.modulate = Color.HOT_PINK
 
-# Rotate the sprite by 15° every second.
+# Rotate the entity by 15° every second.
 func spr_rotate_rotate_process(_delta: float) -> void:
-	sprite.rotation_degrees += 15 * _delta
+	entity.rotation_degrees += 15 * _delta
 
 # Go back to idling when [SPACE] is pressed.	
 func spr_rotate_rotate_unhandled_key_input(event: InputEvent) -> void:
