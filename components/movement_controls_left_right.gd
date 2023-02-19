9d9c342c9bb1fb5549277bf16fb2375b8c0997af
bluematt <307723+bluematt@users.noreply.github.com>
2023-02-19 12:41:28 +0000

    Fixed assert message string interpolation
    Concatenated strings have lower precedence than % string interpolation.  Fix that.  Also explicitly cast the results of `get_path()` to strings.

--- components/movement_controls_left_right.gd
+++ components/movement_controls_left_right.gd
@@ -26,25 +26,25 @@ const DEFAULT_ACTION_RIGHT := "ui_right"
 func _ready() -> void:
 	if velocity_node == null:
 		velocity_node = get_parent() as MovementVelocity
-	assert(velocity_node, "No velocity_node:MovementVelocity component " + 
+	assert(velocity_node, ("No velocity_node:MovementVelocity component " + 
 		"specified in %s. Select one, or reparent this component as a child " +
-		"of a MovementVelocity component." % [get_path()])
+		"of a MovementVelocity component.") % [str(get_path())])
 
 	if input_action_left == null:
 		input_action_left = InputEventAction.new()
 		input_action_left.set_action(DEFAULT_ACTION_LEFT)
-	assert(input_action_left, "No input_action_left:InputEventAction " +
-		"resource specified in %s." % [get_path()])
-	assert(input_action_left.action, "No input_action_left.action " + 
-		"specified in %s." % [get_path()])
+	assert(input_action_left, ("No input_action_left:InputEventAction " +
+		"resource specified in %s.") % [str(get_path())])
+	assert(input_action_left.action, ("No input_action_left.action " + 
+		"specified in %s.") % [str(get_path())])
 
 	if input_action_right == null:
 		input_action_right = InputEventAction.new()
 		input_action_right.set_action(DEFAULT_ACTION_RIGHT)
-	assert(input_action_right, "No input_action_right:InputEventAction " +
-		"resource specified in %s." % [get_path()])
-	assert(input_action_right.action, "No input_action_right.action " + 
-		"specified in %s." % [get_path()])
+	assert(input_action_right, ("No input_action_right:InputEventAction " +
+		"resource specified in %s.") % [str(get_path())])
+	assert(input_action_right.action, ("No input_action_right.action " + 
+		"specified in %s.") % [str(get_path())])
 
 
 func _process(_delta: float) -> void:
