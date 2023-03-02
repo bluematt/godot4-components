3974a3825a5fc5103f2ef0d89462dfe0c31421c9
bluematt <307723+bluematt@users.noreply.github.com>
2023-03-02 12:07:34 +0000

    Updated FlashOnHit
    Updated component to descend from Flash.  Added some getter/setter.  Updated example.

--- addons/bb-components/effect/flash_on_hit.gd
+++ addons/bb-components/effect/flash_on_hit.gd
@@ -1,53 +1,30 @@
 @icon("./flash.svg")
 class_name BBFlashOnHit
-extends Node
+extends BBFlash
 
-## Add a flash effect to a [Node2D], triggered by a collision with a [BBHurtbox] component.
+## Add a [BBFlash] effect to a [Node2D], triggered by a collision with a [BBHurtbox] component.
 ##
 ## @tutorial(Documentation): https://github.com/bluematt/godot4-components/blob/main/doc/flash_on_hit.md
 
-## The node to flash.
-@export var target_node : Node2D
-
 ## The [BBHurtbox] component which notifies when to flash.
-@export var hurtbox_node : Node2D
-
-## The duration of the flash (in seconds).
-@export var flash_duration := 0.2
-
-## The node's default color modulation.
-@export var default_modulation := Color.WHITE
-
-## The node's flash color modulation.
-@export var flash_modulation := Color.RED
-
-## The saturation multiplier for the flash color.
-@export var saturation := 1.0
-
-## The [BBFlash] component used to flash the [member target_node].
-@onready var __flash:BBFlash
+@export var hurtbox_node : BBHurtbox
 
 func _ready() -> void:
-	if target_node == null:
-		target_node = get_parent() as Node2D
-	assert(target_node, ("No target_node:Node2D node specified in %s. Select " +
-		"one, or reparent this component as a child of a Node2D node.") 
-		% [str(get_path())])
-
+	# Call [member BBFlash._ready].
+	super()
+	
 	if hurtbox_node == null:
 		hurtbox_node = get_parent() as BBHurtbox
 	assert(hurtbox_node, ("No hurtbox_node:BBHurtbox component " + 
 		"specified in %s. Select one, or reparent this component as a child " +
 		"of a BBHurtbox component.") % [str(get_path())])
 
-	__flash = BBFlash.new()
-	__flash.target_node = target_node
-	__flash.flash_duration = flash_duration
-	__flash.default_modulation = default_modulation
-	__flash.flash_modulation = flash_modulation
-	__flash.saturation = saturation
+	hurtbox_node.damaged.connect(func(_damage: float): flash())
 
-	add_child(__flash)
+## Set the [BBHurtbox] node.
+func set_hurtbox(new_hurtbox : BBHurtbox):
+	hurtbox_node = new_hurtbox
 
-	hurtbox_node.damaged.connect(func(_damage: float):
-		__flash.flash())
+## Get the [BBHurtbox] node.
+func get_hurtbox() -> BBHurtbox:
+	return hurtbox_node
