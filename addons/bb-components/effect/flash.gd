5ad13424c4d2dad54425c83276cef333af2c5c40
bluematt <307723+bluematt@users.noreply.github.com>
2023-03-02 11:53:11 +0000

    Update flash.gd
    Added getters/setters, set lower limit on duration.

--- addons/bb-components/effect/flash.gd
+++ addons/bb-components/effect/flash.gd
@@ -13,7 +13,9 @@ signal flashed()
 @export var target_node : Node2D
 
 ## The duration of the flash (in seconds).
-@export var flash_duration := 0.2
+@export var duration := 0.2:
+	set(d):
+		duration = max(0, d)
 
 ## The node's default color modulation.
 @export var default_modulation := Color.WHITE
@@ -36,5 +38,44 @@ func flash():
 	flashed.emit()
 	target_node.modulate = flash_modulation * saturation
 	var tween := get_tree().create_tween()
-	tween.tween_property(target_node, "modulate", default_modulation, 
-		flash_duration)
+	tween.tween_property(target_node, "modulate", default_modulation, duration)
+
+## Set the target node.
+func set_target(new_target : Node2D) -> void:
+	target_node = new_target
+
+## Get the target node.
+func get_target() -> Node2D:
+	return target_node
+
+## Set the duration of the animation.
+func set_duration(time : float) -> void:
+	duration = time
+
+## Get the duration of the animation.
+func get_duration() -> float:
+	return duration
+
+## Set the default modulation colour.
+func set_default_modulation(new_modulation : Color) -> void:
+	default_modulation = new_modulation
+
+## Get the default modulation colour.
+func get_default_modulation() -> Color:
+	return default_modulation
+
+## Set the flash modulation colour.
+func set_flash_modulation(new_modulation : Color) -> void:
+	flash_modulation = new_modulation
+
+## Get the flash modulation colour.
+func get_flash_modulation() -> Color:
+	return flash_modulation
+
+## Set the saturation.
+func set_saturation(new_saturation : float) -> void:
+	saturation = new_saturation
+
+## Get the saturation.
+func get_saturation() -> float:
+	return saturation
